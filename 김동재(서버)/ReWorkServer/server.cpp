3a187fd4d0c1a4000112fb2faf8c283d74607be9
KimDJ7105 <101383021+KimDJ7105@users.noreply.github.com>
2024-03-09 04:40:42 +0900

    server.h 분리

---
+++ 김동재(서버)/ReWorkServer/server.cpp
@@ -0,0 +1,37 @@
+#pragma once
+#include "server.h"
+
+concurrency::concurrent_unordered_map<int, shared_ptr<SESSION>> players;
+
+void SERVER::do_accept()
+{
+	acceptor_.async_accept(socket_,
+		[this](boost::system::error_code ec)
+		{
+			if (!ec)
+			{
+				int p_id = GetNewClientID();
+				std::cout << "Client " << p_id << " loged in\n";
+				players[p_id] = std::make_shared<SESSION>(std::move(socket_), p_id);
+				players[p_id]->start();
+				do_accept();
+			}
+		});
+}
+
+int SERVER::GetNewClientID()
+{
+	if (g_user_ID >= MAX_USER) {
+		cout << "MAX USER FULL\n";
+		exit(-1);
+	}
+	return g_user_ID++;
+}
+
+
+SERVER::SERVER(boost::asio::io_context& io_service, int port)
+	: acceptor_(io_service, tcp::endpoint(tcp::v4(), port)),
+	socket_(io_service)
+{
+	do_accept();
+}
--- 김동재(서버)/ReWorkServer/server.h
+++ 김동재(서버)/ReWorkServer/server.h
@@ -9,36 +9,10 @@ private:
 	atomic_int g_user_ID;
 
 private:
-	void do_accept()
-	{
-		acceptor_.async_accept(socket_,
-			[this](boost::system::error_code ec)
-			{
-				if (!ec)
-				{
-					int p_id = GetNewClientID();
-					std::cout << "Client " << p_id << " loged in\n";
-					players[p_id] = std::make_shared<SESSION>(std::move(socket_), p_id);
-					players[p_id]->start();
-					do_accept();
-				}
-			});
-	}
-	int GetNewClientID()
-	{
-		if (g_user_ID >= MAX_USER) {
-			cout << "MAX USER FULL\n";
-			exit(-1);
-		}
-		return g_user_ID++;
-	}
+	void do_accept();
+	
+	int GetNewClientID();
 
 public:
-	SERVER(boost::asio::io_context& io_service, int port)
-		: acceptor_(io_service, tcp::endpoint(tcp::v4(), port)),
-		socket_(io_service)
-	{
-		do_accept();
-	}
-
+	SERVER(boost::asio::io_context& io_service, int port);
 };
