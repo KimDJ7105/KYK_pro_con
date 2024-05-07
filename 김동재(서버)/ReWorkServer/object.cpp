0cd747ace9cacdbe635eb78fe2a5f830a36ed4b5
KimDJ7105 <101383021+KimDJ7105@users.noreply.github.com>
2024-05-08 04:43:57 +0900

    오브젝트 클래스 생성자 정의

--- 김동재(서버)/ReWorkServer/object.cpp
+++ 김동재(서버)/ReWorkServer/object.cpp
@@ -1,1 +1,11 @@
 #include "object.h"
+atomic_int g_object_ID = MAX_USER + 1;
+OBJECT::OBJECT(int type)
+{
+	obj_id = g_object_ID;
+	obj_type = type;
+
+	approx_pos[0] = -1; approx_pos[1] = -1;
+
+	g_object_ID++;
+}
--- 김동재(서버)/ReWorkServer/object.h
+++ 김동재(서버)/ReWorkServer/object.h
@@ -3,8 +3,14 @@
 
 class OBJECT
 {
+public :
 	int obj_id;
 	int obj_type;
+
+	OBJECT(int type);
+
+private:
+	int approx_pos[2]{ -1,-1 };
 
 	std::array<float, 3> pos;
 	std::array<float, 3> rot;
