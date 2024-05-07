61b9081a5de8b68dbd42fc4c7f2e64bc8a0695f2
KimDJ7105 <101383021+KimDJ7105@users.noreply.github.com>
2024-05-08 04:48:26 +0900

    카드키를 생성하도록 설정
    서버가 시작되기 전 카드키 3개를 랜덤한 위치에 생성하도록 구현
    
    -> 차후 GAME 클래스가 생성될때 카드키가 생성되어 해당 게임에서만 사용 가능하도록 수정 예정

--- 김동재(서버)/ReWorkServer/ServerMain.cpp
+++ 김동재(서버)/ReWorkServer/ServerMain.cpp
@@ -2,9 +2,12 @@
 #include "stdafx.h"
 
 #include "server.h"
+#include "object.h"
 
 const auto X_START_POS = 4;
 const auto Y_START_POS = 4;
+
+array<shared_ptr<OBJECT>, 3> Cardkeys;
 
 void Init_Server()
 {
@@ -19,6 +22,20 @@ void worker_thread(boost::asio::io_context* service)
 
 int main()
 {
+	std::cout << "초기 카드키 생성중" << std::endl;
+	Cardkeys[0] = std::make_shared<OBJECT>(OT_KEYCARD);
+	Cardkeys[1] = std::make_shared<OBJECT>(OT_KEYCARD);
+	Cardkeys[2] = std::make_shared<OBJECT>(OT_KEYCARD);
+	
+	Cardkeys[0]->select_pos();
+	Cardkeys[1]->select_pos();
+	Cardkeys[2]->select_pos();
+	std::cout << "카드키 생성 완료" << std::endl;
+
+	/*Cardkeys[0]->show_approx_pos();
+	Cardkeys[1]->show_approx_pos();
+	Cardkeys[2]->show_approx_pos();*/
+
 	boost::asio::io_context io_service;
 	vector <thread > worker_threads;
 	SERVER s(io_service, MY_PORT);
@@ -28,3 +45,4 @@ int main()
 	for (auto i = 0; i < 4; i++) worker_threads.emplace_back(worker_thread, &io_service);
 	for (auto& th : worker_threads) th.join();
 }
+
