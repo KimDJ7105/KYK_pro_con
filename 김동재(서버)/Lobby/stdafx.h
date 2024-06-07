97923b5a782092967ff3771f312632426c8699be
Kim Dong Jae <rlaehdwo1225@naver.com>
2024-06-07 15:20:21 +0900

    lobby server project 생성 및 boost asio연결

---
+++ 김동재(서버)/Lobby/stdafx.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include <thread>
+#include <atomic>
+#include <concurrent_unordered_map.h>
+#include <array>
+#include <random>
+
+#include <boost/asio.hpp>
+
+using namespace std;
+using boost::asio::ip::tcp;
