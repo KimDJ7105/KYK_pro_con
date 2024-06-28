721285e1da6629cb6c844dd0253e48e7cf93fae1
sill0106 <sill0106@naver.com>
2024-06-28 21:07:52 +0900

    누락된 주석 추가

--- 양정우(클라이언트)/DirectX12/Game/Engine/LobbyCameraScript.cpp
+++ 양정우(클라이언트)/DirectX12/Game/Engine/LobbyCameraScript.cpp
@@ -20,8 +20,9 @@ void LobbyCameraScript::LateUpdate()
 	if (INPUT->GetButtonDown(KEY_TYPE::SPACEBAR))
 	{
 		GET_SINGLE(SceneManager)->LoadMainScene(L"TestScene");
+
+
+		//당근칼
+		// 아래에는 Main Scene과 연결할 Main Server에 대한 코드
 	}
-
-
-	//당근칼
 }
